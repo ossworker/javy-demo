1c3f2d997941e99b60f7d9b9f03f64de926ab99e
luanfeng <wbhz_luanfeng@meituan.com>
2023-12-25 18:54:57 +0800

    init

---
+++ Makefile
@@ -0,0 +1,2 @@
+
+opt: wizer --allow-wasi --wasm-bulk-memory true -o ./javy-demo.wasm ./target/wasm32-wasi/release/javy-demo.wasm
---
+++ src/handler.mjs
@@ -0,0 +1,18 @@
+export default {
+    async handler(input, {dayjs, Big, moment}) {
+        console.log('input', input);
+        const momentValid = typeof moment === 'function' && Object.keys(moment).includes('isDayjs');
+        const dayjsValid = typeof dayjs === 'function' && Object.keys(moment).includes('isDayjs');
+        const bigjsValid = typeof Big === 'function';
+
+        return {
+            momentValid,
+            dayjsValid,
+            bigjsValid,
+            bigjsTests: [
+                Big(0.1).add(0.2).eq(0.3),
+                Big(123.12).mul(0.1).round(2).eq(12.31),
+            ]
+        };
+    }
+};
